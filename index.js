0729b3e817cd22046ea0c0e56d34b61184ba3f8a
Shivika <shivikadhodra@gmail.com>
2026-02-10 12:25:08 +0530

    fix: gemini model name

--- index.js
+++ index.js
@@ -126,7 +126,8 @@ case "ai":
 
   try {
     const aiResponse = await axios.post(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${GEMINI_API_KEY}`,
+      
+`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`,
       {
         contents: [
           {
